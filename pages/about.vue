7b343262f9ddc1d122df1fdb11a282d83810c9db
Nikita Sotnikov <nsotnikov16@yandex.ru>
2022-01-16 23:43:51 +0500

    update

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h1>About Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quos
+      ratione delectus est dolore quisquam ex atque molestias, eligendi
+      doloribus odio vitae quo labore repudiandae dicta. Nostrum ea commodi
+      quis!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui earum,
+      similique sapiente libero, officiis sunt sint nemo iusto, assumenda modi
+      error. Sit aliquid provident inventore et molestias magnam autem nostrum.
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
